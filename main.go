bd9e265f94038baf815169f121eebeec39dc502f
DianaMoor <princestaDiana@gmail.com>
2024-10-13 16:20:05 +0200

    Add new handler healthz

--- main.go
+++ main.go
@@ -4,26 +4,48 @@ import (
 	// "fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 )
 
-
-
 func main() {
-	
+
 	mux := http.NewServeMux()
 	serverStruct := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-		ReadTimeout: 2 * time.Second,
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  2 * time.Second,
 		WriteTimeout: 2 * time.Second,
-		IdleTimeout: 2 * time.Second,
+		IdleTimeout:  2 * time.Second,
 	}
 
-	mux.Handle("GET /", http.FileServer(http.Dir(".")))
-	mux.Handle("GET /assets/", http.FileServer(http.Dir(".")))
-	
+	clearTerminal()
+
+	mux.Handle("GET /app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("GET /healthz/", healthz)
+
 	log.Printf("Serving on port: %s\n", serverStruct.Addr)
 	log.Fatal(serverStruct.ListenAndServe())
 
+}
+
+func clearTerminal() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+
+// healthz is a simple health check handler
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
